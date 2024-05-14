c06b2dc3b144c6bef66cdc5c0ed9e92f3665dd5d
Romain <romain.bourdain93@gmail.com>
2024-05-14 16:01:08 +0200

    v0.1

--- script.py
+++ script.py
@@ -2,9 +2,11 @@ from ollama_client.client import OllamaClient
 
 api = OllamaClient("http://192.168.10.4:11435")
 
-api.pull("llama2")
+res = api.pull("llama3:70b", stream=True)
+for part in res:
+    print(part)
 
-response = api.generate("llama2", "Why is the sky blue", stream=True)
+response = api.generate("llama3:70b", "Why is the sky blue", stream=True)
 
 for message in response:
     print(message["response"], end="")
